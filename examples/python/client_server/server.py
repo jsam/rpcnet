38bb3453c3618acea0ae9a6f4f4da7b819bad378
sam <contact@justsam.io>
2025-11-21 19:59:54 +0100

    ci: fixes for the ci

--- examples/python/client_server/server.py
+++ examples/python/client_server/server.py
@@ -5,7 +5,6 @@ import os
 import time
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent / "python"))
 sys.path.insert(0, str(Path(__file__).parent / "generated"))
 
 import rpcnet
